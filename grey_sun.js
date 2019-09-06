e54a6fa4ee57d6ad783ab032b5e68b00fe309325
silpian <26121765+silpian@users.noreply.github.com>
2019-09-06 15:31:47 -0500

    button

--- grey_sun.js
+++ grey_sun.js
@@ -3,6 +3,3 @@ document.body.style.filter = document.body.style.filter + ' grayscale(100%)';
 
 // Safari 6.0 - 9.0
 document.body.style.WebkitFilter = document.body.style.WebkitFilter + ' grayscale(100%)';
-
-// slower method because it requires iteration
-//Array.from(document.getElementsByTagName('*')).forEach(element => element.style.filter = 'grayscale(100%)');
